726b20536277eed5d981c6b6dd2f5c33b284d143
SAURABH <saurabh@SAURABHs-MacBook-Air.local>
2019-01-30 21:53:39 +0530

    updated

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 
 const apiRouter = require("./api");
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 
 
